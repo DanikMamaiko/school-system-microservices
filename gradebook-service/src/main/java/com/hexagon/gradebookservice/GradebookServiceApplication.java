05609fa848cbe0c44e94b7a63fa187c95a92be85
user <danikmamaiko@gmail.com>
2025-09-25 19:48:22 +0300

    add implenetation for new services

---
+++ gradebook-service/src/main/java/com/hexagon/gradebookservice/GradebookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hexagon.gradebookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradebookServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GradebookServiceApplication.class, args);
+    }
+}
+
+
